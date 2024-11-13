4d5e637b36fe01e7b878a0aa4dfe34f282139ef0
Wyatt Markham <wymark@colostate.edu>
2024-11-13 12:53:20 -0700

    Forgot to add Output dir for predictions

--- RFM/forest/Run.sh
+++ RFM/forest/Run.sh
@@ -14,7 +14,7 @@ if [[ "$choice" == "Train" && "$data" == "s" ]]; then
 elif [[ "$choice" == "Train" && "$data" == "r" ]]; then
     spark-submit --class com.forest.RandomForest --master spark://$master.cs.colostate.edu:$port target/forest-1.0-SNAPSHOT.jar hdfs://$master.cs.colostate.edu:$hport/CS435-Term-Project/Data/* Model
 elif [[ "$choice" == "Test" && "$data" == "s" ]]; then
-    spark-submit --class com.forest.TestModel --master spark://$master.cs.colostate.edu:$port target/forest-1.0-SNAPSHOT.jar hdfs://$master.cs.colostate.edu:$hport/CS435-Term-Project/Data_sample/* Model
+    spark-submit --class com.forest.TestModel --master spark://$master.cs.colostate.edu:$port target/forest-1.0-SNAPSHOT.jar hdfs://$master.cs.colostate.edu:$hport/CS435-Term-Project/Data_sample/* Model Predictions
 elif [[ "$choice" == "Test" && "$data" == "r" ]]; then
-    spark-submit --class com.forest.TestModel --master spark://$master.cs.colostate.edu:$port target/forest-1.0-SNAPSHOT.jar hdfs://$master.cs.colostate.edu:$hport/CS435-Term-Project/Data/* Model
+    spark-submit --class com.forest.TestModel --master spark://$master.cs.colostate.edu:$port target/forest-1.0-SNAPSHOT.jar hdfs://$master.cs.colostate.edu:$hport/CS435-Term-Project/Data/* Model Predictions
 fi
